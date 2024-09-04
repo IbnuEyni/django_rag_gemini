21f1bafa164663001bc2fae6c4eea1d9db9a8400
IbnuEyni <shuaibahmedin@gmail.com>
2024-09-04 15:11:08 +0300

    rag_in django

---
+++ rag_gemini_qa_app/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from rag_gemini_qa_app import views
+
+urlpatterns = [
+    path("", views.main ,name="upload"),
+    # path('ask/', views.ask_question, name='ask_question'),
+
+]
